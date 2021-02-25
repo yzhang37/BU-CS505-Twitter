d61574486136d7f995e50f163ce766d0b1b2f939
X230大青椒 <yinzh@wolai.com>
2021-02-26 00:39:49 +0800

    feat: update the code with scrapy from tweets

---
+++ config.py
@@ -0,0 +1,35 @@
+import json
+
+
+class MyAuth:
+    def __init__(self):
+        try:
+            with open("key.json", 'r') as fkey:
+                data = json.load(fkey)
+                self.__API_KEY = data["api_key"]
+                self.__API_SECRET = data["api_secret"]
+                self.__ACCESS_TOKEN = data["access_token"]
+                self.__ACCESS_TOKEN_SECRET = data["access_token_secret"]
+                self.__BEAR = data["bear"]
+        except FileNotFoundError:
+            print("key.json not found!!!")
+            raise
+
+    @property
+    def api_key(self):
+        return self.__API_KEY
+
+    @property
+    def api_secret(self):
+        return self.__API_SECRET
+
+    @property
+    def access_token(self):
+        return self.__ACCESS_TOKEN
+
+    @property
+    def access_token_secret(self):
+        return self.__ACCESS_TOKEN_SECRET
+
+
+auth = MyAuth()
---
+++ main.py
@@ -0,0 +1,62 @@
+from config import auth
+from twython import Twython
+import json
+import os
+import time
+
+twitter = Twython(
+    app_key=auth.api_key, app_secret=auth.api_secret,
+    oauth_token=auth.access_token, oauth_token_secret=auth.access_token_secret
+)
+# twitter.verify_credentials()
+
+
+def fetch_twitter(count=10):
+    results = twitter.search(
+        q="covid",
+        lang="en",
+        result_type="recent",
+        tweet_mode="extended",
+        count=str(count)
+    )
+
+    def extract_text(status):
+        if "retweeted_status" in status.keys():
+            return status["retweeted_status"]["full_text"]
+        else:
+            return status["full_text"]
+
+    result_texts = list(map(extract_text, results["statuses"]))
+    return result_texts
+
+
+def save_raw_twitter():
+    raw_texts = fetch_twitter(10000)
+    with open('raw_tweets.json', 'w') as output:
+        json.dump(raw_texts, output, indent=2, ensure_ascii=False)
+
+
+def continuous_save_twitter():
+    raw_json_file = 'raw_tweets.json'
+    raw_json_file = os.path.abspath(os.path.join('.', raw_json_file))
+
+    raw_texts = []
+    if os.path.exists(raw_json_file) and os.path.isfile(raw_json_file):
+        with open(raw_json_file, 'r') as fin:
+            raw_texts = json.load(fin)
+    count = len(raw_texts)
+    if count > 0:
+        print(f"Preloaded {count} texts.")
+
+    while count < 10000:
+        new_texts = fetch_twitter(100)
+        raw_texts.extend(new_texts)
+        count = len(raw_texts)
+        print(f"Outputting {count} texts...")
+        with open(raw_json_file, 'w') as fout:
+            json.dump(raw_texts, fout, indent=2, ensure_ascii=False)
+        time.sleep(10)
+
+
+if __name__ == "__main__":
+    continuous_save_twitter()
